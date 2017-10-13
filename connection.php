357a5757546ac086b12b0a8c1da8bfb21dbc29b6
Unknown <masitingss@gmail.com>
2017-10-13 16:26:22 +0700

    Coba
    Coba coba

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
   $servername = 'localhost';
   $username = 'root';
   $password = 'qwerty123';
-  $db = 'tabungankus';
+  $db = 'tabungankuss';
 
   $conn = new mysqli($servername, $username, $password, $db);
   if ($conn->connect_error) {
